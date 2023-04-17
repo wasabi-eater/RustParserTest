7989333b89780cef96f88ae862b91e4dd62358ee
wasabi-eater <88137260+wasabi-eater@users.noreply.github.com>
2023-04-17 23:11:04 +0900

    Update parser2.rs

--- parser2.rs
+++ parser2.rs
@@ -1,3 +1,9 @@
+use core::fmt::Formatter;
+use core::fmt::Debug;
+use core::ops::Range;
+use core::ops::Index;
+use std::rc::Rc;
+
 type Parser<S, A, E> = Box<dyn FnOnce(S) -> (S, Result<A, E>)>;
 #[macro_export]
 macro_rules! parser {
@@ -32,32 +38,116 @@ pub fn try_parse<S: 'static, A: 'static, E: 'static>(parser: Parser<S, A, E>) ->
         (state, Ok(result))
     })
 }
-pub fn expect<S, E: 'static>(cond: impl 'static + FnOnce(&S) -> bool, error: E) -> Parser<Vec<S>, S, E> {
+pub fn many<S: 'static, A: 'static, E: 'static>(parser: impl 'static + Fn() -> Parser<S, A, E>) -> Parser<S, Vec<A>, E> {
+    fn inner<S: 'static, A: 'static, E: 'static>(parser: impl 'static + Fn() -> Parser<S, A, E>, mut vec: Vec<A>) -> Parser<S, Vec<A>, E> {
+        parser! {
+            let! result = try_parse(parser());
+            (match result{
+                Ok(value) => inner(parser, {vec.push(value); vec}),
+                Err(_) => ret(vec)
+            })
+        }
+    }
+    inner(parser, vec![])
+}
+#[derive(Clone)]
+pub struct RcQueue<A> {
+    slice: Rc<[A]>,
+    offset: usize,
+    length: usize
+}
+impl<A: Debug> Debug for RcQueue<A>{
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        self.as_slice().fmt(formatter)
+    }
+}
+impl<A: PartialEq> PartialEq for RcQueue<A> {
+    fn eq(&self, right: &Self) -> bool {
+        self.as_slice() == right.as_slice()
+    }
+}
+impl<A> RcQueue<A> {
+    pub fn as_slice(&self) -> &[A] {
+        &self.slice[self.offset..self.offset + self.length]
+    }
+    pub fn get(&self, ind: usize) -> Option<&A> {
+        self.as_slice().get(ind)
+    }
+    pub fn len(&self) -> usize {
+        self.length
+    }
+    pub fn pop_front(&mut self) -> Option<&A> {
+        self.offset += 1;
+        if self.length > 0 {
+            self.length -= 1;
+            Some(&self.slice[self.offset - 1])
+        }
+        else {
+            None
+        }
+    }
+    pub fn pop_back(&mut self) -> Option<&A> {
+        if self.length > 0 {
+            self.length -= 1;
+            Some(&self.slice[self.offset + self.length])
+        }
+        else {
+            None
+        }
+    }
+}
+
+impl<'a, A: 'static + Clone> From<&'a [A]> for RcQueue<A> {
+    fn from(slice: &[A]) -> Self {
+        Self {slice: slice.into(), offset: 0, length: slice.len()}
+    }
+}
+impl<A> Index<usize> for RcQueue<A> {
+    type Output = A;
+    fn index(&self, ind: usize) -> &A {
+        self.get(ind).expect("Index out of range")
+    }
+}
+impl<A> Index<Range<usize>> for RcQueue<A> {
+    type Output = [A];
+    fn index(&self, ind: Range<usize>) -> &[A] {
+        let Range{ end, .. } = ind;
+        if end < self.length {
+            &self.as_slice()[ind]
+        }
+        else {
+            panic!("Index out of range")
+        }
+    }
+}
+
+pub fn expect<S: Clone, E: 'static>(cond: impl 'static + FnOnce(&S) -> bool, error: E) -> Parser<RcQueue<S>, S, E> {
     Box::new(|mut state| {
-        match state.pop() {
+        let state_ = state.clone();
+        match state.pop_front() {
             None => (state, Err(error)),
             Some(last) =>
                 if cond(&last) {
+                    let last = last.clone();
                     (state, Ok(last))
                 } else {
-                    state.push(last);
-                    (state, Err(error))
+                    (state_, Err(error))
                 }
         }
     })
 }
-fn create_parser() -> Parser<Vec<char>, (), &'static str> {
+fn create_parser() -> Parser<RcQueue<char>, (), &'static str> {
     parser!{
         let! _ = expect(|x| *x == 'H', "Not Matched Error");
-        let! _ = expect(|x| *x == 'E', "Not Matched Error");
+        let! _ = many(|| expect(|x| *x == 'E', "Not Matched Error"));
         let! _ = expect(|x| *x == 'Y', "Not Matched Error");
         ret(())
     }
 }
 fn main() {
-    let vec: Vec<_> = "HEY".chars().rev().collect();
-    assert_eq!(create_parser()(vec), (vec![], Ok(())));
-    
-    let vec: Vec<_> = "Hi".chars().rev().collect();
-    assert_eq!(create_parser()(vec), (vec!['i'], Err("Not Matched Error")));
+    let vec: RcQueue<char> = "HEEEY".chars().collect::<Vec<_>>().as_slice().into();
+    assert_eq!(create_parser()(vec), (RcQueue::from(&[] as &[char]), Ok(())));
+
+    let vec: RcQueue<char> = "Hi".chars().collect::<Vec<_>>().as_slice().into();
+    assert_eq!(create_parser()(vec), (RcQueue::from(&['i'] as &[char]), Err("Not Matched Error")));
 }
